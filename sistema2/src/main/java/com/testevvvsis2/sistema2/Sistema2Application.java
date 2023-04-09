3097b3ec6dd1e3b0441f5da72d36ef441414a3d3
Marcio Gabriel <gabriel09msn@gmail.com>
2023-04-09 14:01:36 -0300

    Update

--- sistema2/src/main/java/com/testevvvsis2/sistema2/Sistema2Application.java
+++ sistema2/src/main/java/com/testevvvsis2/sistema2/Sistema2Application.java
@@ -2,12 +2,8 @@ package com.testevvvsis2.sistema2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-
-import com.testevvvsis2.sistema2.config.AppConfig;
 
 @SpringBootApplication
-@Import(AppConfig.class)
 public class Sistema2Application {
 
 	public static void main(String[] args) {
--- sistema2/src/main/java/com/testevvvsis2/sistema2/service/ModalService.java
+++ sistema2/src/main/java/com/testevvvsis2/sistema2/service/ModalService.java
@@ -3,7 +3,6 @@ package com.testevvvsis2.sistema2.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.http.HttpHeaders;
@@ -15,9 +14,6 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class ModalService {
-
-    @Autowired
-    private RestTemplate restTemplate;
 
     private final Client client;
 
@@ -52,26 +48,6 @@ public class ModalService {
             })
             .subscribe();
 
-        /*String url = "https://teste-vvv-production.up.railway.app/modal";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<Modal> requestEntity = new HttpEntity<>(modal, headers);
-
-        ResponseEntity<Modal> responseEntity = restTemplate.postForEntity(url, requestEntity, Modal.class);
-
-        if(responseEntity.getStatusCode() == HttpStatus.OK) {
-
-            Modal responseBody = responseEntity.getBody();
-            System.out.println("Resposta: " + responseBody);
-
-        } else {
-
-            System.out.println("Erro na resposta: " + responseEntity.getStatusCode());
-
-        }*/
-
     }
 
     public String atualizarModal(Long id, Modal newModal) {
@@ -89,27 +65,5 @@ public class ModalService {
         }
 
     }
-
-    /*public void atualizarStatus(Long id, Modal modal) {
-
-        String url = "https://teste-vvv-production.up.railway.app/modal/"+id;
-
-        Modal modalAtualizado = restTemplate.getForObject(url, Modal.class, id);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<Modal> requestEntity = new HttpEntity<>(modalAtualizado, headers);
-
-        ResponseEntity<Modal> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Modal.class, id);
-
-        if (responseEntity.getStatusCode() == HttpStatus.OK) {
-            Modal responseBody = responseEntity.getBody();
-            System.out.println("Resposta: " + responseBody);
-        } else {
-            System.out.println("Erro na resposta: " + responseEntity.getStatusCode());
-        }
-
-    }*/
 
 }
